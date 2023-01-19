bec9770e33c2311c792e14cf5e62af5be2364e8f
Markus Piotrowski <Markuspiotrowski@live.com>
2023-01-19 11:52:50 +0900

    Add different groups

--- hubs/ChatHub.cs
+++ hubs/ChatHub.cs
@@ -4,9 +4,9 @@ namespace BlazorServerSignalRApp.Server.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message, string timestamp, string messageId)
+        public async Task SendMessage(string user, string message, string timestamp, string group,string messageId)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message, timestamp, messageId);
+            await Clients.All.SendAsync("ReceiveMessage", user, message, timestamp, group,messageId);
         }
 
         public async Task SendReaction(string reaction, string messageId, string user){
