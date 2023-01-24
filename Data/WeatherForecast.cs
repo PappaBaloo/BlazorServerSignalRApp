59cabcfe5431d92fa8a5772a1f1a66d6dfe72bf6
PappaBaloo <thebettercharlie@gmail.com>
2023-01-24 14:04:24 +0900

    new

--- Data/WeatherForecast.cs
+++ Data/WeatherForecast.cs
@@ -2,7 +2,7 @@ namespace BlazorServerSignalRApp.Data;
 
 public class WeatherForecast
 {
-    public DateOnly Date { get; set; }
+    public DateTime Date { get; set; }
 
     public int TemperatureC { get; set; }
 
--- Data/WeatherForecastService.cs
+++ Data/WeatherForecastService.cs
@@ -7,7 +7,7 @@ public class WeatherForecastService
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
+    public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
     {
         return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
